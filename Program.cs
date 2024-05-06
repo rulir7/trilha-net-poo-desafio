9394ad75af63c5649b0ccdc4b8f59da9f745fc27
rulir7 <rulir@hotmail.com>
2024-05-06 15:43:24 -0300

    commit inicial

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// Criando um objeto da classe Nokia
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 128);
+
+// Testando os métodos da classe Nokia
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+// Criando um objeto da classe Iphone
+Iphone iphone = new Iphone("987654321", "Iphone X", "ABCDEFGHIJKLMNOP", 256);
+
+// Testando os métodos da classe Iphone
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("FaceTime");
+
+Console.WriteLine("Testes concluídos. Pressione qualquer tecla para sair...");
+Console.ReadKey();
